35e1409433b4ea7ea17af6693162efb2aa10b299
bermu <bermu@localhost.localdomain>
2025-09-05 02:21:18 -0500

    correccion ligera 7

--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-// lib/auth.ts
-import { prisma } from './db';
-import { prismaAdapter } from '@better-auth/prisma-adapter';
-import { betterAuth } from 'better-auth';
-
-export const auth = betterAuth({
-  database: prismaAdapter(prisma),
-  // resto de la configuración…
-});
